11444662bdc8b03a1042923af49c1e44ee490e51
beatrimt <beatrice.m.trascau@ntnu.no>
2024-06-14 17:16:47 +0200

    Code to convert to sf and plot

--- scripts/3_arenicola_map.R
+++ scripts/3_arenicola_map.R
@@ -12,6 +12,8 @@ library(terra)
 library(sf)
 library(ggplot2)
 library(ggpubr)
+library(plotly)
+library(scales)
 
 # 1. LOAD DATA -----------------------------------------------------------------
 
@@ -31,6 +33,9 @@ arenicola <- arenicola |>
 # Check values for depth
 levels(as.factor(arenicola$depth_m))
 
+# Check species names
+unique(arenicola$x_species) # spaces at the ends of names - need to be removed
+
 # Write function to replace values contain "-" with the larger number
 replace_depth_values <- function(value) {
   if (value == "" | value == "-") {
@@ -46,17 +51,52 @@ replace_depth_values <- function(value) {
 
 # Apply function to column depth
 arenicola_clean <- arenicola %>%
-  mutate(depth_m    = sapply(depth_m, replace_depth_values))
+  mutate(depth_m    = sapply(depth_m, replace_depth_values),
+         x_species = str_trim(x_species)) |>
+  # remove rows with species name different from Arenicola marina and Arenicolides ecaudata
+  filter(!x_species %in% c("Arenicola sp.", "Arenicolidae"))
 
 # Check if it worked
 levels(as.factor(arenicola_clean$depth_m)) #looks ok
+unique(arenicola_clean$x_species)
 
 ## 1.2. Get Norway shapefile ---------------------------------------------------
+
+norway <- geodata::gadm(country = "NOR", level = 0, 
+                        path = tempdir(),
+                        version = "latest")
+# Check shapefile
+plot(norway)
+
+# Convert norway to sf object
+norway_sf <- st_as_sf(norway)
 
 ## 1.3. Convert occurrences to spatial df --------------------------------------
 
 # Check column names
 colnames(arenicola_clean)
 
-# Convert the 
-arenicola_sf <- st_as_sf(arenicola_clean,coords=c("decimalLongitude","decimalLatitude"),crs=crs(globalbiomes))
+# Convert to spatial datafram for plotting
+arenicola_sf <- st_as_sf(arenicola_clean,
+                         coords = c("longitude","latitude"),
+                         crs = crs(norway_sf))
+# 3. PLOT ----------------------------------------------------------------------  
+
+# Plot map with two panels
+arenicola_map1 <- ggplot() +
+  geom_sf(data = norway_sf, fill = "lightgray", color = "white") +
+  geom_sf(data = arenicola_sf, aes(color = depth_m)) +
+  facet_wrap(~ x_species) +
+  scale_color_gradient(low = "blue", high = "red") +
+  theme_classic() +
+  labs(color = "Depth (m)") +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        legend.position = "bottom")
+
+# Save to file
+ggsave(here("figures", "arenicola_map1.png"),
+       width=13, height=9)
+
+# Inspect maps with plotly
+#plotly(arenicola_map1)
