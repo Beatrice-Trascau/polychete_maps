7908ce6b1af35b1c33c9baacb1fcc4da53112243
beatrimt <beatrice.m.trascau@ntnu.no>
2024-06-17 17:12:13 +0200

    Plotted maps with north arrow and scale

--- scripts/3_arenicola_map.R
+++ scripts/3_arenicola_map.R
@@ -14,13 +14,14 @@ library(ggplot2)
 library(ggpubr)
 library(plotly)
 library(scales)
+library(ggspatial)
 
 # 1. LOAD DATA -----------------------------------------------------------------
 
 ## 1.1. Clean data -------------------------------------------------------------
 
 # Read data in
-arenicola <- read.csv(here("data", "raw_data", "Arenicola_Table1supppl.csv"),
+arenicola <- read.csv(here("data", "raw_data", "Table1_submit.csv"),
                                header = TRUE, sep = ";")
 
 # Check data
@@ -82,8 +83,64 @@ arenicola_sf <- st_as_sf(arenicola_clean,
                          crs = crs(norway_sf))
 # 3. PLOT ----------------------------------------------------------------------  
 
+## 3.1. Plot two separate maps -------------------------------------------------
+
+# Plot map for A. marina
+arenicola_marina <- arenicola_sf |>
+  filter(x_species == "Arenicola marina") |>
+  ggplot() +
+  geom_sf(data = norway_sf, fill = "lightgray", color = "white") +
+  geom_sf(data = arenicola_sf, aes(color = depth_m)) +
+  annotation_north_arrow(location = "br", which_north = "true",
+                         pad_y = unit(0.8, "cm"),
+                         style = north_arrow_fancy_orienteering) +
+  annotation_scale(location = "br", width_hint = 0.35) +
+  scale_color_gradient(low = "blue", high = "red") +
+  theme_classic() +
+  labs(color = "Depth (m)",
+       title = "Arenicola marina") +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        legend.position = "none",
+        plot.title = element_text(face = "italic"))
+
+# Plot map for A. ecaudata
+arenicolides_ecaudata <- arenicola_sf |>
+  filter(x_species == "Arenicolides ecaudata") |>
+  ggplot() +
+  geom_sf(data = norway_sf, fill = "lightgray", color = "white") +
+  geom_sf(data = arenicola_sf, aes(color = depth_m)) +
+  scale_color_gradient(low = "blue", high = "red") +
+  theme_classic() +
+  labs(color = "Depth (m)",
+       title = "Arenicolides ecaudata") +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        legend.position = "none",
+        plot.title = element_text(face = "italic"),
+        axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.line.y = element_blank())
+
+# Extract legend from one of the plots
+legend <- get_legend(arenicola_marina + theme(legend.position = "bottom"))
+
+# Arrange the two plots in a single figure
+arenicola_map1 <- plot_grid(arenicola_marina, arenicolides_ecaudata,
+                            ncol = 2, align = "hv", rel_heights = c(1, 1))
+
+# Add legend to bottom centre
+arenicola_final_plot <- plot_grid(arenicola_map1, legend, ncol = 1, 
+                        rel_heights = c(1, 0.1))
+
+# Save to file
+ggsave(here("figures", "arenicola_map1.png"),
+       width=13, height=9)
+
+## 3.2. Plot with facet wrap ---------------------------------------------------
 # Plot map with two panels
-arenicola_map1 <- ggplot() +
+arenicola_map2 <- ggplot() +
   geom_sf(data = norway_sf, fill = "lightgray", color = "white") +
   geom_sf(data = arenicola_sf, aes(color = depth_m)) +
   facet_wrap(~ x_species) +
@@ -95,8 +152,7 @@ arenicola_map1 <- ggplot() +
         legend.position = "bottom")
 
 # Save to file
-ggsave(here("figures", "arenicola_map1.png"),
+ggsave(here("figures", "arenicola_map2.png"),
        width=13, height=9)
 
-# Inspect maps with plotly
-#plotly(arenicola_map1)
+# END OF SCRIPT ----------------------------------------------------------------
